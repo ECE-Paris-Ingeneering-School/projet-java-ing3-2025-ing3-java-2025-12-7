d325ec13f0176d3d1f78d7c22abe77f83124db6d
Laure Petit <laure.petit@edu.ece.fr>
2025-04-20 13:17:06 +0200

    Ajout du main appelant à la vue pour se connecter

--- Shopping/src/Controleur/Main.java
+++ Shopping/src/Controleur/Main.java
@@ -1,10 +1,24 @@
 package Controleur;
+import DAO.DAOFactory;
+import DAO.DAOFormulaireIMPL;
 import Vue.Mywindow;
+import Vue.VueConnexion;
+import Vue.VueInscription;
+
 import javax.swing.*;
+
 
 public class Main {
     public static void main(String[] args) throws UnsupportedLookAndFeelException {
-        SwingUtilities.invokeLater(Mywindow::new);
+        //SwingUtilities.invokeLater(Mywindow::new); fenetre de l'accueil à décommenter si besoin
+        DAOFactory dao = DAOFactory.getInstance("shopping", "root", "root");
+        DAOFormulaireIMPL daoFormulaire = new DAOFormulaireIMPL(dao);
 
+        JFrame parent = new JFrame();
+        parent.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        parent.setVisible(false);
+
+        //Appel de la fenetre de connexion
+        new VueConnexion(parent, daoFormulaire);
     }
 }
